5837060d1c3f2f11aa38694d160fe2cbea8c95ed
Nathali Alves <system.nathy@hotmail.com>
2025-01-11 14:43:46 -0400

    Criação do modelo Etapa e implementação da etapa de Recebimento

--- core/models.py
+++ core/models.py
@@ -95,3 +95,10 @@ class Material(models.Model):
         Retorna uma representação em string do material, incluindo nome e tipo.
         """
         return f"{self.nome} - {self.tipo}"
+
+class Etapa(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+
+    def __str__(self):
+        return self.nome
